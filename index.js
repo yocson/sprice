4e8d69d9ef575dbb589b84f486689e9f51b92d4f
yocson <yocson@gmail.com>
2018-06-18 20:01:55 -0400

    add commander and co prompt

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
 console.log('Hello, world!');
+const program = require("commander");
+const co = require("co");
+const prompt = require('co-prompt');
+
+program
+  .arguments('<file>')
+  .option('-u, --username <username>', 'The user to atuhenticate as')
+  .option('-p, --password <password>', 'The user\'s password')
+  .action(function(file) {
+    co(function *() {
+            let username = yield prompt('username: ');
+            let password = yield prompt.password('password: ');
+            console.log('user: %s pass: %s file: %s',
+                username, password, file);
+    });
+  })
+  .parse(process.argv);
