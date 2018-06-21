e9bb59ab581db65c586b2c7f555539ecfb32f691
yocson <yocson@gmail.com>
2018-06-20 22:08:21 -0400

    add company

--- index.js
+++ index.js
@@ -21,15 +21,55 @@ function priceQuery(symbol, args) {
     });
 }
 
+function companyQuery(symbol, args) {
+    https.get(`https://api.iextrading.com/1.0/stock/${symbol}/company`, (res) => {
+        // console.log('statusCode:', res.statusCode);
+        if (res.statusCode === 200) {
+            // res.setEncoding('utf8');
+            res.on('data', (d) => {
+                console.log(colors.green('Company: '));
+                // process.stdout.write(d);
+                let info = JSON.parse(d);
+                console.log(info);
+                console.log(info.companyName);
+            }); 
+        }
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
+
+function newsQuery(symbol, args) {
+    https.get(`https://api.iextrading.com/1.0/stock/${symbol}/news`, (res) => {
+        console.log('statusCode:', res.statusCode);
+        if (res.statusCode === 200) {
+            res.setEncoding('utf8');
+            res.on('data', (d) => {
+                console.log(colors.green('News: '));
+                process.stdout.write(d);
+                let info = JSON.parse(d);
+                console.log(info);
+            }); 
+        }
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
+
 program
   .command('price <symbol>')
   .alias('p')
-  .option('-p --price', 'Query price of compony')
-  .option('-c --componey', 'Componey information')
   .action(priceQuery);
   
 program
-  .command('')
+  .command('company <symbol>')
+  .alias('c')
+  .action(companyQuery);
+
+program
+  .command('news <symbol>')
+  .alias('n')
+  .action(newsQuery);
 
 program.parse(process.argv);
 
