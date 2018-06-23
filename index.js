1dba20d7839a4c15f09c01e0928b9d01273c3ac9
Yocson Wang <yocson@outlook.com>
2018-06-22 20:34:39 -0400

    refact code

--- index.js
+++ index.js
@@ -3,57 +3,40 @@ const program = require("commander"),
       co = require("co"),
       prompt = require('co-prompt'),
       https = require('https'),
-      colors = require('colors');
+      colors = require('colors'),
+      concatStream = require('concat-stream');
 
-
-function priceQuery(symbol, args) {
-    https.get(`https://api.iextrading.com/1.0/stock/${symbol}/price`, (res) => {
-        // console.log('statusCode:', res.statusCode);
+function fetchContent(url) {
+    https.get(url, (res) => {
         if (res.statusCode === 200) {
-            res.on('data', (d) => {
-                console.log(colors.green('Price: '));
-                process.stdout.write(d);
-                console.log('');
-            }); 
+            if (res.statusCode === 200) {
+                res.setEncoding('utf8');
+                res.pipe(concatStream((data) => {
+                    console.log(data);
+                }));
+            }
         }
     }).on('error', (e) => {
         console.error(e);
     });
+}
+
+function priceQuery(symbol, args) {
+    const url = `https://api.iextrading.com/1.0/stock/${symbol}/price`;
+    console.log(colors.green('Price: '));
+    fetchContent(url);
 }
 
 function companyQuery(symbol, args) {
-    https.get(`https://api.iextrading.com/1.0/stock/${symbol}/company`, (res) => {
-        // console.log('statusCode:', res.statusCode);
-        if (res.statusCode === 200) {
-            // res.setEncoding('utf8');
-            res.on('data', (d) => {
-                console.log(colors.green('Company: '));
-                // process.stdout.write(d);
-                let info = JSON.parse(d);
-                console.log(info);
-                console.log(info.companyName);
-            }); 
-        }
-    }).on('error', (e) => {
-        console.error(e);
-    });
+    const url = `https://api.iextrading.com/1.0/stock/${symbol}/company`;
+    console.log(colors.green('Company: '));
+    fetchContent(url);
 }
 
 function newsQuery(symbol, args) {
-    https.get(`https://api.iextrading.com/1.0/stock/${symbol}/news`, (res) => {
-        console.log('statusCode:', res.statusCode);
-        if (res.statusCode === 200) {
-            res.setEncoding('utf8');
-            res.on('data', (d) => {
-                console.log(colors.green('News: '));
-                process.stdout.write(d);
-                let info = JSON.parse(d);
-                console.log(info);
-            }); 
-        }
-    }).on('error', (e) => {
-        console.error(e);
-    });
+    const url = `https://api.iextrading.com/1.0/stock/${symbol}/news`;
+    console.log(colors.green('News: '));
+    fetchContent(url);
 }
 
 program
